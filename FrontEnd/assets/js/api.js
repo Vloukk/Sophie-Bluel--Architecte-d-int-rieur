bcd0a5abad86cfa6c9dd90c5dabd25bb8d3e0aa5
Vloukk <v.valentin.evangelista@gmail.com>
2024-01-29 12:20:57 +0100

    first commit

--- FrontEnd/assets/js/api.js
+++ FrontEnd/assets/js/api.js
@@ -30,16 +30,22 @@ export function fetchCategories() {
       });
 }
 
-//récupère les données login à partir de l'API
-export function fetchLogin() {
-  return fetch('http://localhost:5678/api/users/login')
-      .then(response => response.json())
-      .catch(error => {
-          console.error('Erreur lors de la récupération des catégories :', error);
-          throw error;
-      });
-}
 
+// Verifie la connexion au Back-end
+const backendURL = 'http://localhost:5678/api-docs/';
+
+fetch(backendURL)
+  .then(response => {
+    if (!response.ok) {
+      // Si la réponse n'est pas OK, l'utilisateur n'est pas connecté
+      alert("Vous n'êtes pas connecté au serveur backend.");
+    } 
+  })
+  .catch(error => {
+    // En cas d'erreur, l'utilisateur n'est pas connecté
+    alert("Erreur de connexion au serveur backend. Veuillez réessayer plus tard.");
+    console.error("Erreur de connexion au serveur backend:", error);
+  });
 
 
 
